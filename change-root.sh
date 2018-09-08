1117e3bb551c4bb456802d48523c4c9d27e01554
ayaka209 <weloveayaka@gmail.com>
2018-09-09 01:35:26 +0800

    change document root.
    change document root.

---
+++ change-root.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#https://stackoverflow.com/questions/38578190/change-document-root-with-command-lines
+
+replace_string () {
+    while :; do
+        case $1 in
+               file=?*) local    file=${1#*=} ;;
+            replace=?*) local replace=${1#*=} ;;
+               with=?*) local    with=${1#*=} ;;
+                     *) break                 ;;
+        esac
+        shift
+    done
+
+    sudo sed -i -- "s/$replace/$with/ig" $file
+}
+
+replace_string    file='/etc/apache2/sites-enabled/000-default.conf' \
+               replace='.*DocumentRoot.*' \
+                  with='DocumentRoot /var/www/html/public'
+
+
+#replace_string    file='/etc/apache2/apache2.conf' \
+#               replace='.*DocumentRoot.*' \
+#                  with='DocumentRoot "path-to-your-document-root"'
+
+service apache2 reload
