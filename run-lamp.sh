d08ac9d3a9d580f2d0cd7f7927099c3fd6bcff23
ayaka209 <weloveayaka@gmail.com>
2019-03-14 03:52:43 +0800

    Update run-lamp.sh

--- run-lamp.sh
+++ run-lamp.sh
@@ -61,9 +61,13 @@ fi
 # Run MariaDB
 /usr/bin/mysqld_safe --timezone=${DATE_TIMEZONE}&
 
+/usr/sbin/service cron start
+/usr/sbin/service supervisor start
+
 # Run Apache:
 if [ $LOG_LEVEL == 'debug' ]; then
     /usr/sbin/apachectl -DFOREGROUND -k start -e debug
 else
     &>/dev/null /usr/sbin/apachectl -DFOREGROUND -k start
 fi
+
